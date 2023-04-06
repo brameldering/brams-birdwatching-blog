7c8dd8f5016caed1be487c4e0df3aa492f9371f8
brameldering <bram.eldering@gmail.com>
2023-04-06 09:37:20 +0200

    Front end template

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,6 +1,23 @@
+import { useParams } from "react-router-dom";
+import NotFoundPage from "./NotFoundPage";
+import articles from "./article-content";
+
 const ArticlePage = () => {
+    // const params = useParams();
+    // const articleId = params.articleId;
+    const { articleId } = useParams();
+    const article = articles.find(article => article.name === articleId);
+
+    if (!article) {
+        return <NotFoundPage />
+    }
     return (
-        <h1>This is the article page!</h1>
+        <>
+            <h1>{article.title}</h1>
+            {article.content.map((paragraph,i) => (
+                <p key={i}>{paragraph}</p>
+            ))}
+        </>
     );
 }
 
