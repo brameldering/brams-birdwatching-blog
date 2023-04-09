d71b222f4160b340ead823c95388f97f853b6127
brameldering <bram.eldering@gmail.com>
2023-04-09 13:37:06 +0200

    Added axios and get

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,24 +1,38 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 import NotFoundPage from "./NotFoundPage";
 import articles from "./article-content";
+import CommentsList from "./Components/CommentsList";
 
 const ArticlePage = () => {
-    // const params = useParams();
-    // const articleId = params.articleId;
-    const { articleId } = useParams();
-    const article = articles.find(article => article.name === articleId);
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+  const { articleId } = useParams();
 
-    if (!article) {
-        return <NotFoundPage />
-    }
-    return (
-        <>
-            <h1>{article.title}</h1>
-            {article.content.map((paragraph,i) => (
-                <p key={i}>{paragraph}</p>
-            ))}
-        </>
-    );
-}
+  useEffect(() => {
+    const loadArticleInfo = async () => {
+      const response = await axios.get(`/api/articles/${articleId}`);
+      const newArticleInfo = response.data;
+      setArticleInfo(newArticleInfo);
+    };
+    loadArticleInfo();
+  }, []);
+
+  const article = articles.find((article) => article.name === articleId);
+
+  if (!article) {
+    return <NotFoundPage />;
+  }
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <p>This article has {articleInfo.upvotes} upvote(s)</p>
+      {article.content.map((paragraph, i) => (
+        <p key={i}>{paragraph}</p>
+      ))}
+      <CommentsList comments={articleInfo.comments} />
+    </>
+  );
+};
 
 export default ArticlePage;
