2769ab6094593d1b01d4831e4ef564f084b667de
brameldering <bram.eldering@gmail.com>
2023-04-10 13:48:32 +0200

    Add comment form

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -2,37 +2,56 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import NotFoundPage from "./NotFoundPage";
-import articles from "./article-content";
 import CommentsList from "./Components/CommentsList";
+import AddCommentForm from "./Components/AddCommentForm";
+import articles from "./article-content";
 
 const ArticlePage = () => {
-  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
-  const { articleId } = useParams();
+    const [articleInfo, setArticleInfo] = useState({
+        upvotes: 0,
+        comments: [],
+    });
+    const { articleId } = useParams();
 
-  useEffect(() => {
-    const loadArticleInfo = async () => {
-      const response = await axios.get(`/api/articles/${articleId}`);
-      const newArticleInfo = response.data;
-      setArticleInfo(newArticleInfo);
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        };
+        loadArticleInfo();
+    }, []);
+
+    const article = articles.find((article) => article.name === articleId);
+
+    const addUpvote = async () => {
+        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const updatedArticle = response.data;
+        setArticleInfo(updatedArticle);
     };
-    loadArticleInfo();
-  }, []);
 
-  const article = articles.find((article) => article.name === articleId);
-
-  if (!article) {
-    return <NotFoundPage />;
-  }
-  return (
-    <>
-      <h1>{article.title}</h1>
-      <p>This article has {articleInfo.upvotes} upvote(s)</p>
-      {article.content.map((paragraph, i) => (
-        <p key={i}>{paragraph}</p>
-      ))}
-      <CommentsList comments={articleInfo.comments} />
-    </>
-  );
+    if (!article) {
+        return <NotFoundPage />;
+    }
+    return (
+        <>
+            <h1>{article.title}</h1>
+            <div className="upvotes-section">
+                <button onClick={addUpvote}>Upvote</button>
+                <p>This article has {articleInfo.upvotes} upvote(s)</p>
+            </div>
+            {article.content.map((paragraph, i) => (
+                <p key={i}>{paragraph}</p>
+            ))}
+            <AddCommentForm
+                articleName={articleId}
+                onArticleUpdated={(updatedArticle) =>
+                    setArticleInfo(updatedArticle)
+                }
+            />
+            <CommentsList comments={articleInfo.comments} />
+        </>
+    );
 };
 
 export default ArticlePage;
---
+++ src/settings/blogsettings.js
@@ -0,0 +1,13 @@
+const dateOptions = {
+  weekday: "short",
+  year: "numeric",
+  month: "short",
+  day: "numeric",
+  hour12: false,
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  timeZoneName: "short",
+};
+
+export default dateOptions;
