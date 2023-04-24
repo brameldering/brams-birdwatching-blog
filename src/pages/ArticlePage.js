c5efda4a17e48bb6f0b3645b5a486a3337eaf8e5
brameldering <bram.eldering@gmail.com>
2023-04-24 11:45:14 +0200

    Optimized code and commented console.logs

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -7,140 +7,137 @@ import AddCommentForm from "./Components/AddCommentForm";
 import useUser from "../hooks/useUser";
 import articles from "./article-content";
 
-let callCountPage = 0;
-let callCountUseEffect = 1;
+// let callCountPage = 0;
+// let callCountUseEffect = 1;
 
 const ArticlePage = () => {
-    const [articleInfo, setArticleInfo] = useState({
-        upvotes: 0,
-        comments: [],
-        canUpvote: false,
-    });
-    const { canUpvote } = articleInfo;
-    const { articleId } = useParams();
+  const [articleInfo, setArticleInfo] = useState({
+    upvotes: 0,
+    comments: [],
+    canUpvote: false,
+  });
+  const { canUpvote } = articleInfo;
+  const { articleName } = useParams();
 
-    const { user, isLoading } = useUser();
-    callCountPage++;
+  const { user, isLoading } = useUser();
 
-    console.log("==== after init ArticlePage  ==== " + callCountPage);
-    console.log("articleInfo: ");
-    console.log(articleInfo);
-    console.log("articleId: " + articleId);
-    console.log("user: ");
-    console.log(user);
-    console.log("isLoading " + isLoading);
-    console.log("");
+  //   callCountPage++;
+  //   console.log("==== after init ArticlePage  ==== " + callCountPage);
+  //   console.log("articleInfo: ");
+  //   console.log(articleInfo);
+  //   console.log("articleId: " + articleName);
+  //   console.log("user: ");
+  //   console.log(user);
+  //   console.log("isLoading " + isLoading);
+  //   console.log("");
 
-    useEffect(() => {
-        const loadArticleInfo = async () => {
-            const token = user && (await user.getIdToken());
-            // Test ======================
-            console.log("==== user.getIdToken ====");
-            console.log("token: ");
-            console.log(token);
-            console.log("user: ");
-            console.log(user);
-            console.log("");
-            // ===========================
-            // if token has a value then set headers to contain token otherwise set headers to empty object
-            // (otherwise headers will be a null string)
-            const headers = token ? { authtoken: token } : {};
-            console.log("==== before get article ====");
-            const response = await axios.get(`/api/articles/${articleId}`, {
-                headers,
-            });
-            // Test ======================
-            console.log("==== after get article ====");
-            console.log("headers: ");
-            console.log(headers);
-            console.log("response: ");
-            console.log(response);
-            console.log("");
-            // ===========================
+  useEffect(() => {
+    const loadArticleInfo = async () => {
+      const token = user && (await user.getIdToken());
+      // Test ======================
+      //   console.log("==== user.getIdToken ====");
+      //   console.log("token: ");
+      //   console.log(token);
+      //   console.log("user: ");
+      //   console.log(user);
+      //   console.log("");
+      // ===========================
 
-            const newArticleInfo = response.data;
-            // Test ======================
-            console.log("==== newArticleInfo ====");
-            console.log("newArticleInfo: ");
-            console.log(newArticleInfo);
-            console.log("");
-            // ===========================
+      // if token has a value then set headers to contain token otherwise set headers to empty object
+      // (otherwise headers will be a null string)
+      const headers = token ? { authtoken: token } : {};
+      const response = await axios.get(`/api/articles/${articleName}`, {
+        headers,
+      });
+      // Test ======================
+      //   console.log("==== after get article ====");
+      //   console.log("headers: ");
+      //   console.log(headers);
+      //   console.log("response: ");
+      //   console.log(response);
+      //   console.log("");
+      // ===========================
 
-            setArticleInfo(newArticleInfo);
-        };
-        if (!isLoading) {
-            loadArticleInfo();
-        }
-        callCountUseEffect++;
-    }, [isLoading, articleId, user]);
+      const newArticleInfo = response.data;
+      // Test ======================
+      //   console.log("==== newArticleInfo ====");
+      //   console.log("newArticleInfo: ");
+      //   console.log(newArticleInfo);
+      //   console.log("");
+      // ===========================
 
-    console.log("==== before find article ====");
-    const article = articles.find((article) => article.name === articleId);
+      setArticleInfo(newArticleInfo);
+    };
+    if (!isLoading) {
+      loadArticleInfo();
+    }
+    //callCountUseEffect++;
+  }, [isLoading, articleName, user]);
+
+  const article = articles.find((article) => article.name === articleName);
+  //   console.log("==== after find article ====");
+  //   console.log("article: ");
+  //   console.log(article);
+  //   console.log("");
+  // ===========================
+
+  const addUpvote = async () => {
+    const token = user && (await user.getIdToken());
+    const headers = token ? { authtoken: token } : {};
+
     // Test ======================
-    console.log("==== after find article ====");
-    console.log("article: ");
-    console.log(article);
-    console.log("");
+    // console.log("==== addUpvote ====");
+    // console.log("token: ");
+    // console.log(token);
+    // console.log("headers: ");
+    // console.log(headers);
+    // console.log("");
     // ===========================
 
-    const addUpvote = async () => {
-        const token = user && (await user.getIdToken());
-        const headers = token ? { authtoken: token } : {};
+    const response = await axios.put(`/api/articles/${articleName}/upvote`, null, { headers });
+    const updatedArticle = response.data;
+    // Test ======================
+    // console.log("==== after put ====");
+    // console.log("response: ");
+    // console.log(response);
+    // console.log("updatedArticle: ");
+    // console.log(updatedArticle);
+    // console.log("");
+    // ===========================
+    setArticleInfo(updatedArticle);
+  };
 
-        // Test ======================
-        console.log("==== addUpvote ====");
-        console.log("token: ");
-        console.log(token);
-        console.log("headers: ");
-        console.log(headers);
-        console.log("");
-        // ===========================
+  if (!article) {
+    return <NotFoundPage />;
+  }
 
-        const response = await axios.put(`/api/articles/${articleId}/upvote`, null, { headers });
-        const updatedArticle = response.data;
-        // Test ======================
-        console.log("==== after put ====");
-        console.log("response: ");
-        console.log(response);
-        console.log("updatedArticle: ");
-        console.log(updatedArticle);
-        console.log("");
-        // ===========================
-        setArticleInfo(updatedArticle);
-    };
-
-    if (!article) {
-        return <NotFoundPage />;
-    }
-    console.log("Before return ArticlePage");
-    console.log("");
-    return (
-        <>
-            <h1>{article.title}</h1>
-            <p>callCountPage: {callCountPage}</p>
-            <p>callCountUseEffect: {callCountUseEffect}</p>
-            <div className="upvotes-section">
-                {user ? (
-                    <button onClick={addUpvote}>{canUpvote ? "Upvote" : "Already Upvoted"}</button>
-                ) : (
-                    <button>Log in to upvote</button>
-                )}
-                <p>This article has {articleInfo.upvotes} upvote(s)</p>
-            </div>
-            {article.content.map((paragraph, i) => (
-                <p key={i}>{paragraph}</p>
-            ))}
-            {user ? (
-                <AddCommentForm
-                    articleName={articleId}
-                    onArticleUpdated={(updatedArticle) => setArticleInfo(updatedArticle)}
-                />
-            ) : (
-                <button>Log in to add a comment</button>
-            )}
-            <CommentsList comments={articleInfo.comments} />
-        </>
-    );
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <div className='upvotes-section'>
+        {user ? (
+          <button onClick={addUpvote} disabled={!canUpvote}>
+            {canUpvote ? "Upvote" : "Already Upvoted"}
+          </button>
+        ) : (
+          <button>Log in to upvote</button>
+        )}
+        <p>This article has {articleInfo.upvotes} upvote(s)</p>
+      </div>
+      {article.content.map((paragraph, i) => (
+        <p key={i}>{paragraph}</p>
+      ))}
+      {user ? (
+        <AddCommentForm
+          articleName={articleName}
+          onArticleUpdated={(updatedArticle) => setArticleInfo(updatedArticle)}
+        />
+      ) : (
+        <button>Log in to add a comment</button>
+      )}
+      <CommentsList comments={articleInfo.comments} />
+    </>
+  );
 };
 
 export default ArticlePage;
--- src/pages/Components/AddCommentForm.js
+++ src/pages/Components/AddCommentForm.js
@@ -3,95 +3,88 @@ import axios from "axios";
 import useUser from "../../hooks/useUser.js";
 import dateOptions from "../../settings/blogsettings.js";
 
-let callCountAddCommentForm = 0;
-let callCountAddCommentFunction = 0;
+// let callCountAddCommentForm = 0;
+// let callCountAddCommentFunction = 0;
 
 const AddCommentForm = ({ articleName, onArticleUpdated }) => {
-    const [name, setName] = useState("");
-    const [commentText, setCommentText] = useState("");
-    const { user } = useUser();
+  const [commentText, setCommentText] = useState("");
+  const { user } = useUser();
 
-    callCountAddCommentForm++;
+  // Test ======================
+  //   callCountAddCommentForm++;
+  //   console.log("==== after init AddCommentForm ==== " + callCountAddCommentForm);
+  //   console.log("name: " + name);
+  //   console.log("commentText: " + commentText);
+  //   console.log("user: ");
+  //   console.log(user);
+  //   console.log("");
+  // ===========================
+
+  const addComment = async () => {
+    const postedDateTime = new Date().toLocaleString("nl-NL", dateOptions);
+
+    const token = user && (await user.getIdToken());
+    const headers = token ? { authtoken: token } : {};
+
     // Test ======================
-    console.log("==== after init AddCommentForm ==== " + callCountAddCommentForm);
-    console.log("name: " + name);
-    console.log("commentText: " + commentText);
-    console.log("user: ");
-    console.log(user);
-    console.log("");
+    // callCountAddCommentFunction++;
+    // console.log("==== addComment init ==== " + callCountAddCommentFunction);
+    // console.log("user: ");
+    // console.log(user);
+    // console.log("token: ");
+    // console.log(token);
+    // console.log("headers: ");
+    // console.log(headers);
+    // console.log(
+    //   `=== before addComment post for article ${articleName}, postedBy: ${name} and postedDateTime: ${postedDateTime}`
+    // );
+    // console.log("");
     // ===========================
 
-    const addComment = async () => {
-        const postedDateTime = new Date().toLocaleString("nl-NL", dateOptions);
-
-        const token = user && (await user.getIdToken());
-        const headers = token ? { authtoken: token } : {};
-
-        callCountAddCommentFunction++;
-
-        // Test ======================
-        console.log("==== addComment init ==== " + callCountAddCommentFunction);
-        console.log("user: ");
-        console.log(user);
-        console.log("token: ");
-        console.log(token);
-        console.log("headers: ");
-        console.log(headers);
-
-        console.log(
-            `=== before addComment post for article ${articleName}, postedBy: ${name} and postedDateTime: ${postedDateTime}`
-        );
-        console.log("");
-        // ===========================
-
-        const response = await axios.post(
-            `/api/articles/${articleName}/comments`,
-            {
-                postedBy: name,
-                postedDateTime: postedDateTime,
-                text: commentText,
-            },
-            {
-                headers,
-            }
-        );
-
-        // Test ======================
-        console.log(
-            `=== after addComment post for article ${articleName}, postedBy: ${name} and postedDateTime: ${postedDateTime}`
-        );
-        console.log("");
-        // ======================
-        const updatedArticle = response.data;
-        // Test ======================
-        console.log("updatedArticle: ");
-        console.log(updatedArticle);
-        console.log("");
-        // ======================
-
-        onArticleUpdated(updatedArticle);
-
-        setName("");
-        setCommentText("");
-    };
-
-    return (
-        <div id="add-comment-form">
-            <h3>Add a Comment</h3>
-            <p>callCountAddCommentForm: {callCountAddCommentForm}</p>
-            <p>callCountAddCommentFunction: {callCountAddCommentFunction}</p>
-            {user && <p>You are posting as {user.email}</p>}
-            <label>
-                <textarea
-                    value={commentText}
-                    onChange={(e) => setCommentText(e.target.value)}
-                    rows="4"
-                    cols="50"
-                />
-            </label>
-            <button onClick={addComment}>Add Comment</button>
-        </div>
+    const response = await axios.post(
+      `/api/articles/${articleName}/comments`,
+      {
+        postedDateTime: postedDateTime,
+        text: commentText,
+      },
+      {
+        headers,
+      }
     );
+
+    // Test ======================
+    // console.log(
+    //   `=== after addComment post for article ${articleName} and postedDateTime: ${postedDateTime}`
+    // );
+    // console.log("");
+    // ======================
+    const updatedArticle = response.data;
+    // Test ======================
+    // console.log("updatedArticle: ");
+    // console.log(updatedArticle);
+    // console.log("");
+    // ======================
+
+    onArticleUpdated(updatedArticle);
+
+    setCommentText("");
+  };
+
+  return (
+    <div id='add-comment-form'>
+      <h3>Add a Comment</h3>
+      {user && <p>You are posting as {user.email}</p>}
+      <label>
+        <textarea
+          value={commentText}
+          onChange={(e) => setCommentText(e.target.value)}
+          rows='4'
+          cols='50'
+        />
+      </label>
+      <button onClick={addComment}>Add Comment</button>
+    </div>
+  );
 };
 
 export default AddCommentForm;
